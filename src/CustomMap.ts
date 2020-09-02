1f845ef74bc3c7b4c1a1898ef8bab4e7d5553613
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2020-09-02 12:34:56 -0400

    Creates User and Company with mockup data and shows marker and info on map

---
+++ src/CustomMap.ts
@@ -0,0 +1,33 @@
+import { Company } from './Company';
+import { User } from './User';
+import { Mappable } from './Mappable';
+export class CustomMap {
+  private map: google.maps.Map;
+
+  constructor(divId: string) {
+    this.map = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.map,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+      animation: google.maps.Animation.DROP,
+    });
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'hi there' + mappable.name,
+      });
+      infoWindow.open(this.map, marker);
+    });
+  }
+}
